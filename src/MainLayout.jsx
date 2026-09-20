eeab53c254150d21d14c6944059d3053fbc0874d
aveng <avenger.wlc@gmail.com>
2026-09-20 17:50:22 -0400

    fixed footer in the middle of page and hoefully fixed pictures

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -10,15 +10,19 @@ export function MainLayoutContainer({ children, className = '' }) {
 
 export default function MainLayout() {
   const location = useLocation()
-  const hideFooter = location.pathname === '/resume'
+  const isResume = location.pathname === '/resume'
 
   return (
-    <div className="font-body flex h-svh w-full flex-col bg-white text-black antialiased dark:bg-black dark:text-white">
+    <div
+      className={`font-body flex w-full flex-col bg-white text-black antialiased dark:bg-black dark:text-white ${
+        isResume ? 'h-svh' : 'min-h-svh'
+      }`}
+    >
       <NavBar />
       <main className="flex min-h-0 w-full min-w-0 flex-1 flex-col">
         <Outlet />
       </main>
-      {!hideFooter && <Footer />}
+      {!isResume && <Footer />}
     </div>
   )
 }
