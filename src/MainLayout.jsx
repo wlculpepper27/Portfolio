fc912550cefb7e015007e32c4a26c49ff8bc2760
aveng <avenger.wlc@gmail.com>
2026-09-20 23:05:09 -0400

    starting to invert the colors of the site

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,27 +2,17 @@ import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './components/UI/Footer.jsx'
 import NavBar from './components/UI/NavBar.jsx'
 
-export function MainLayoutContainer({ children, className = '' }) {
-  return (
-    <div className={`mx-auto w-full max-w-7xl px-8 ${className}`.trim()}>{children}</div>
-  )
-}
-
 export default function MainLayout() {
   const location = useLocation()
-  const isResume = location.pathname === '/resume'
+  const hideFooter = location.pathname === '/resume'
 
   return (
-    <div
-      className={`font-body flex w-full flex-col bg-white text-black antialiased dark:bg-black dark:text-white ${
-        isResume ? 'h-svh' : 'min-h-svh'
-      }`}
-    >
+    <div className="font-body flex h-svh w-full flex-col bg-black text-white antialiased">
       <NavBar />
       <main className="flex min-h-0 w-full min-w-0 flex-1 flex-col">
         <Outlet />
       </main>
-      {!isResume && <Footer />}
+      {!hideFooter && <Footer />}
     </div>
   )
 }
